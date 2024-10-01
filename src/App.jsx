8c0d2b861487d9c874be3cf315578600c42a5460
rimfat1311 <simontimnan@gmail.com>
2024-10-01 14:56:05 -0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Homepage from './pages/Homepage'
+import Previewpage from './pages/Previewpage'
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Homepage />
+      <Previewpage />
     </div>
   )
 }
