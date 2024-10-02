9ec511cca16a73cc3cfd0447fdb7989e57f333f6
rimfat1311 <simontimnan@gmail.com>
2024-10-02 06:57:40 -0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,13 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Homepage from './pages/Homepage'
 import Previewpage from './pages/Previewpage'
-import { Router, Route } from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
       <Navbar />
-      <Router>
-        <Route path='homepage' element={<Homepage />}/>
-        <Router path='previewpage' element={<Previewpage />}/>
-      </Router>
+      <Homepage />
+      <Previewpage />
     </div>
   )
 }
