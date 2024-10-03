46b4dcfea84f5f922c6957ad8c22b920b60a3c79
rimfat1311 <simontimnan@gmail.com>
2024-10-03 09:04:50 -0700

    update

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect } from 'react';
-import { IoSearch } from "react-icons/io5";
+import { FaSearch } from "react-icons/fa";
 
 const Homepage = () => {
-  const BASE_URL = 'https://restcountries.com/v3.1/all'; // Make sure this endpoint is correct
+  const BASE_URL = 'https://restcountries.com/v3.1/all'; 
 
   const fetchCountriesData = async () => {
     try {
@@ -11,28 +11,32 @@ const Homepage = () => {
         throw new Error('Network response was not ok');
       }
       const data = await response.json();
-      console.log('Fetched Data:', data); // Log the fetched data
-      // You can also set the fetched data to state if needed
+      console.log('Fetched Data:', data);
     } catch (error) {
       console.error('Error fetching data:', error);
     }
   };
 
   useEffect(() => {
-    fetchCountriesData(); // Call the fetch function when the component mounts
+    fetchCountriesData(); 
   }, []);
 
  
   return (
-    <div className='flex justify-between my-7'>
-      <IoSearch className='relative left-14'/>
+    <div className='flex justify-between my-7 px-16'>
+      <FaSearch className='relative left-14 top-3 z-10'/>
       <input 
       type="text" 
-      className='absolute border shadow-md  mx-16 w-96 px-6 h-10 outline-none'
+      className='absolute border shadow-md  mx- w-96 px-20 h-10 outline-none'
       placeholder='Search for any country....'
       />
-      <select name="filter by region" id="filter by region" className='h-8'>filter by region
+      <select
+       name="filter by region"
+       
+        id="filter by region" 
+        className='h-8'>
         <option value="1">filter by region</option>
+        <option value="2">Africa</option>
       </select>
     </div>
   )
--- src/pages/Previewpage.jsx
+++ src/pages/Previewpage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Previewpage = () => {
   return (
-    <div>Previewpage</div>
+    <div></div>
   )
 }
 
