eb9f096f533b4b9ce4ece5a1e0b2c8a09cbbe972
rimfat1311 <simontimnan@gmail.com>
2024-10-05 22:24:15 -0700

    update

--- src/pages/Previewpage.jsx
+++ src/pages/Previewpage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Previewpage = () => {
   return (
     <div>
-      <div>Previewpage</div>
+      <div>Previewpage goes here with also mapping</div>
     </div>
   )
 }
