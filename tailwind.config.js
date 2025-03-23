c53ff8ef3cd3ce610a8ac5cda246d53d4597fde6
rimfat1311 <simontimnan@gmail.com>
2025-03-23 03:03:29 -0700

    update on the app

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,4 @@
-// /** @type {import('tailwindcss').Config} */
-// export default {
-//   content: [
-//     "./index.html",
-//     "./src/**/*.{js,ts,jsx,tsx}",
-//   ],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// }
 
-
-// tailwind.config.js
 module.exports = {
   darkMode: 'class', // Enables dark mode using a class
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
